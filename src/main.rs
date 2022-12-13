3af8416fedf3d88aea0b0fc27bc878f434a22c7f
Racerroar888 <84479373+Racerroar888@users.noreply.github.com>
2022-12-13 15:22:46 +0000

    Create remove()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn add(mut todo: HashMap<u8, String>, item: String) -> HashMap<u8, String> {
     return todo;
 }
 
+fn remove(mut todo: HashMap<u8, String>, key: &u8) -> HashMap<u8, String> {
+	todo.remove(key);
+	return todo;
+}
+
 fn main() {
     let mut todo: HashMap<u8, String> = HashMap::new();
     let test_string: String = String::from("Hello World!");
@@ -12,4 +17,8 @@ fn main() {
     todo = add(todo, test_string);
 
     println!("{:?}", todo);
+
+		todo = remove(todo, &0);
+
+		println!("{:?}", todo);
 }
