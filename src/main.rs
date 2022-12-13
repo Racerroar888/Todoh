7d08b67f8a9b0bd0674bbf1d30ac08228ef21925
Racerroar888 <84479373+Racerroar888@users.noreply.github.com>
2022-12-13 15:10:42 +0000

    Make add()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
+fn add(mut todo: HashMap<u8, String>, item: String) -> HashMap<u8, String> {
+    todo.insert(todo.len().try_into().unwrap(), item);
+    return todo;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut todo: HashMap<u8, String> = HashMap::new();
+    let test_string: String = String::from("Hello World!");
+
+    todo = add(todo, test_string);
+
+    println!("{:?}", todo);
 }
