05d8efcff756056f78b5ab878eb209ceb9466abb
Racerroar888 <84479373+Racerroar888@users.noreply.github.com>
2022-12-13 20:42:25 +0000

    Make print()

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,24 @@ fn remove(mut todo: HashMap<u8, String>, key: &u8) -> HashMap<u8, String> {
 	return todo;
 }
 
+fn print(todo: &HashMap<u8, String>) {
+	for (key, item) in todo {
+		println!("{key}: {item}")
+	}
+}
+
 fn main() {
     let mut todo: HashMap<u8, String> = HashMap::new();
     let test_string: String = String::from("Hello World!");
+	let test_string_two: String = String::from("Testing");
 
     todo = add(todo, test_string);
+    todo = add(todo, test_string_two);
 
-    println!("{:?}", todo);
+
+		print(&todo);
 
 		todo = remove(todo, &0);
 
-		println!("{:?}", todo);
+		print(&todo);
 }
